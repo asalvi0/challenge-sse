eca2e2ef8dde43c6b2bbca8b51c9f97ea9c914d0
Arturo Salvio <asalvio@outlook.com>
2022-12-21 21:20:39 -0600

    minor code cleanup

--- internal/api/api.go
+++ internal/api/api.go
@@ -21,13 +21,18 @@ func (s *Server) Start() {
 
 	router.GET("/students", students)
 	router.GET("/students/:id", student)
+
 	router.GET("/exams", exams)
 	router.GET("/exams/:id", exam)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", s.Port), router))
 }
 
-func students(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func students(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Students")
+}
+
+func student(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id := params.ByName("id")
 	if len(id) == 0 {
 		fmt.Fprintf(w, "url param 'id' is missing")
@@ -37,7 +42,11 @@ func students(w http.ResponseWriter, r *http.Request, params httprouter.Params) 
 	fmt.Fprintf(w, "Student: %s", id)
 }
 
-func exams(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func exams(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Exams")
+}
+
+func exam(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	number := params.ByName("number")
 	if len(number) == 0 {
 		fmt.Fprintf(w, "url param 'number' is missing")
@@ -45,12 +54,4 @@ func exams(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	}
 
 	fmt.Fprintf(w, "Exam: %s", number)
-}
-
-func student(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	fmt.Fprintf(w, "Students")
-}
-
-func exam(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	fmt.Fprintf(w, "Exams")
 }
